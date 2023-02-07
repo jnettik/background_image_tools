14a1a197826bc298028aec20d6ffa6f2bc0489fb
James Nettik <james.nettik@gmail.com>
2023-02-06 21:32:23 -0700

    Added switcher for File/Media entities.'

--- src/Services/BackgroundMediaRenderer.php
+++ src/Services/BackgroundMediaRenderer.php
@@ -4,7 +4,8 @@ namespace Drupal\background_image_tools\Services;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Component\Uuid\Php;
-use Drupal\media\MediaInterface;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\File\FileUrlGeneratorInterface;
 
 /**
  * Class BackgroundMediaRenderer.
@@ -26,25 +27,36 @@ class BackgroundMediaRenderer {
   protected $uuid;
 
   /**
-   * Constructs a new BackgroundMediaRenderer object.
+   * File Url Generator Service.
+   *
+   * @var \Drupal\Core\File\FileUrlGeneratorInterface
+   */
+  protected $fileGenerator;
+
+  /**
+   * Constructs a new BackgroundsBackgroundImageRenderer object.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The EntityTypeManager object.
    * @param \Drupal\Component\Uuid\Php $uuid
    *   The Uuid object.
+   * @param \Drupal\Core\File\FileUrlGeneratorInterface $file_generator
+   *   The File Generator interface.
    */
   public function __construct(
     EntityTypeManagerInterface $entity_type_manager,
-    Php $uuid
+    Php $uuid,
+    FileUrlGeneratorInterface $file_generator
   ) {
     $this->entityTypeManager = $entity_type_manager;
     $this->uuid = $uuid;
+    $this->fileGenerator = $file_generator;
   }
 
   /**
    * Return the path of the background image.
    *
-   * @param \Drupal\media\MediaInterface $entity
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
    *   The Media entity to use.
    * @param string $image_style
    *   The ImageStyle path to get.
@@ -52,10 +64,19 @@ class BackgroundMediaRenderer {
    * @return string
    *   The file path.
    */
-  protected function getFilePath(MediaInterface $entity, string $image_style) {
-    $fid = $entity->getSource()->getSourceFieldValue($entity);
+  protected function getFilePath(ContentEntityInterface $entity, string $image_style) {
     /** @var \Drupal\file\FileInterface $file */
-    $file = $this->entityTypeManager->getStorage('file')->load($fid);
+    $file = $entity;
+
+    // Check to see if passed entity is a Media entity. If so, get the File
+    // entity we need for processing.
+    if ($entity->getEntityTypeId() == 'media') {
+      /** @var \Drupal\media\MediaInterface $entity */
+      $fid = $entity->getSource()->getSourceFieldValue($entity);
+      /** @var \Drupal\file\FileInterface $file */
+      $file = $this->entityTypeManager->getStorage('file')->load($fid);
+    }
+
     /** @var \Drupal\image\ImageStyleInterface $style */
     $style = $this->entityTypeManager->getStorage('image_style')->load($image_style);
     $file_path = $style->buildUrl($file->getFileUri());
@@ -78,7 +99,7 @@ class BackgroundMediaRenderer {
     // @todo It's possible to pass this off to Twig which would make writing
     // the CSS easier.
     $css = sprintf('%s {', $selector);
-    $css .= sprintf('background-image: url(\'%s\');', file_url_transform_relative($file_path));
+    $css .= sprintf('background-image: url(\'%s\');', $this->fileGenerator->transformRelative($file_path));
     $css .= '}';
 
     return $css;
@@ -89,8 +110,8 @@ class BackgroundMediaRenderer {
    *
    * @param string $selector
    *   The CSS selector to use.
-   * @param \Drupal\media\MediaInterface $entity
-   *   The Media entity to use for the background.
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   *   The Media or File (Image) entity to use for the background.
    * @param string $image_style
    *   The ImageStyle to use for the image.
    *
@@ -99,7 +120,7 @@ class BackgroundMediaRenderer {
    */
   public function getStyles(
     string $selector,
-    MediaInterface $entity,
+    ContentEntityInterface $entity,
     string $image_style
   ) {
     $image_url = $this->getFilePath($entity, $image_style);
